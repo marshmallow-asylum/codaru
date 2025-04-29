0ab6cf9a42e1cf08336a6a18aba929e1b1ead6a6
saejin <smorez.ka@outlook.com>
2025-04-28 21:44:06 -0400

    join for txt file

--- src/emails.ts
+++ src/emails.ts
@@ -3,5 +3,5 @@ import { createClerkClient } from "@clerk/nextjs/server"
 void async function () {
     const clerkClient = createClerkClient({ secretKey: process.env.CLERK_SECRET_KEY })
     const list = await clerkClient.users.getUserList({ orderBy: '-created_at', limit: 250, offset: 0 })
-    console.log(list.data.map(user => user._raw.email_addresses[0].email_address))
+    console.log(list.data.map(user => user._raw.email_addresses[0].email_address).join("\n"))
 }()
